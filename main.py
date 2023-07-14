0752db5874aa8e421b330a008c24e8ac03518709
MACQ25 <24368983-MACQ25@users.noreply.replit.com>
2023-07-14 22:35:03 +0000

    wiped and started again
    Time to read documentation really carefully

--- main.py
+++ main.py
@@ -1,48 +1,42 @@
 import discord
-import os
-import ffmpeg
-import asyncio
 from discord.ext import commands
-from cogs.music_cog import music_cog
-#from help_cog import help_cog
+import os
+#from help_cog import help_cog}
+
+description = "This is Sidney bot's description, make sure to write something relevant here later"
 
 intents = discord.Intents.default()
 intents.messages = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='%', intents=intents)
+sidneyBot = commands.Bot(command_prefix='!',
+                         description=description,
+                         intents=intents)
 
-@bot.event
+
+@sidneyBot.event
 async def on_ready():
-  await bot.add_cog(music_cog(bot))
-  muhCog = bot.get_cog('music_cog')
-  command2 = muhCog.get_commands()
-  print([c.name for c in command2])
-  print('{0.user} says: God save the queen!'.format(bot))
+  print(sidneyBot.get_command("add2"))
+  print(f'{sidneyBot.user} says: God save the queen!')
 
 
-@bot.event
-async def on_message(message):
-
-  #This is for debbugging purposes
-  username = str(message.author)
-  user_message = str(message.content)
-  channel = str(message.channel)
-
-  print(f"{username} said: '{user_message}' ({channel})")
-  # debug ends here
-
-  if message.author == bot.user:
-    return
-  if message.content == "$hello":
-    await message.channel.send("evening")
-
-async def main():
-  await bot.start(os.environ['myToken'])
-  bot.run(os.environ['myToken'], root_logger=True)
+@sidneyBot.event
+async def on_message(msg):
+  if msg.author != sidneyBot.user:
+    print(f'{msg.author}: {msg.content}')
 
 
-asyncio.run(main())
+@sidneyBot.command("salute")
+async def salute(ctx, usr):
+  await ctx.send(f'cheers {usr}')
+
+@sidneyBot.command("add")
+async def add(ctx, left: int, right: int):
+  print("add")
+  await ctx.send(left + right)
+
+
+sidneyBot.run(os.environ['myToken'])
 
 # sauce for async kerfuffle solu: https://stackoverflow.com/questions/71504627/runtimewarning-coroutine-botbase-load-extension-was-never-awaited-after-upd
 
