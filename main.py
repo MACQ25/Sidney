6c36e201f91003e637703e0a5f1d04a1e54d0c49
M. Canul <canulmauricio03@gmail.com>
2023-08-02 19:30:21 -0400

    Made some changes, finally commands pop in into the application

--- main.py
+++ main.py
@@ -1,44 +1,67 @@
 import discord
+from discord import voice_client
 from discord.ext import commands
 import os
-#from help_cog import help_cog}
+import asyncio
+import youtube_dl
+
+# from help_cog import help_cog}
 
 description = "This is Sidney bot's description, make sure to write something relevant here later"
 
 intents = discord.Intents.default()
-intents.messages = True
 intents.message_content = True
+intents.members = True
 
-print("hewwo github")
-
-sidneyBot = commands.Bot(command_prefix='!',
-                         description=description,s
-                         intents=intents)
+sidneyBot = discord.Bot(
+    command_prefix=commands.when_mentioned_or('/'),
+    description=description,
+    intents=intents,
+    debug_guilds=[1124124038634156062]
+)
 
 
 @sidneyBot.event
 async def on_ready():
-  print(sidneyBot.get_command("add2"))
-  print(f'{sidneyBot.user} says: God save the queen!')
+    print(sidneyBot.get_command("add"))
+    print(f'{sidneyBot.user} says: God save the queen!')
 
 
-@sidneyBot.event
-async def on_message(msg):
-  if msg.author != sidneyBot.user:
-    print(f'{msg.author}: {msg.content}')
+@sidneyBot.command(description="Say Hi to another user on the server")
+async def salute(ctx, msg):
+    await ctx.respond(f'cheers {msg}')
 
 
-@sidneyBot.command("salute")
-async def salute(ctx, usr):
-  await ctx.send(f'cheers {usr}')
-
-@sidneyBot.command("add")
+@sidneyBot.command()
 async def add(ctx, left: int, right: int):
-  print("add")
-  await ctx.send(left + right)
+    print("add")
+    await ctx.respond(str(left + right))
 
 
-sidneyBot.run(os.environ['myToken'])
+@sidneyBot.command(name='join', description='Tells the bot to join the voice channel')
+async def join(ctx, *, channel: discord.VoiceChannel):
+    if not ctx.author.voice:
+        await ctx.respond("{} is not connected to a voice channel".format(ctx.author.name))
+    else:
+        if ctx.voice_client is None:
+            await ctx.respond("cheers m8")
+            await channel.connect()
+        else:
+            await ctx.respond("Already in a channel m8")
+
+
+@sidneyBot.command(name='leave', description='Tells the bot to leave the voice channel')
+async def leave(com):
+    if com.voice_client is None:
+        await com.respond("I ain't in a channel ya piker!")
+    else:
+        if com.voice_client.channel is not com.author.voice.channel:
+            await com.respond("You ain't in my channel ya no-hoper!")
+        else:
+            await com.voice_client.disconnect(force=True)
+            await com.respond("done")
+
+sidneyBot.run('MTEyNDEyMDgyNDI0NTM5MTQzNA.GwhfIv.jlGfuQGs3vRk2xCM2xZWAfZvAtRZ-ac1Kg-Blc')
 
 # sauce for async kerfuffle solu: https://stackoverflow.com/questions/71504627/runtimewarning-coroutine-botbase-load-extension-was-never-awaited-after-upd
 
