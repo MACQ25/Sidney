243d9125aa0aa067e672c74b4289645f3bfad0d5
M. Canul <canulmauricio03@gmail.com>
2023-08-04 19:44:17 -0400

    Finally got it playing somethinf, Youtube-dl is givin issues though

--- cogs/MusicCog.py
+++ cogs/MusicCog.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import discord
 import youtube_dl
 from discord.ext import commands
@@ -26,11 +28,31 @@ ffmpeg_options = {"options": "-vn"}
 ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
 
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source: discord.AudioSource, *, data: dict, volume: float = 0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get("title")
+        self.url = data.get("url")
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(
+            None, lambda: ytdl.extract_info(url, download=not stream)
+        )
+        if "entries" in data:
+            data = data["entries"][0]
+
+        filename = data["url"] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+
 class MusicCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @discord.slash_command(name='join', description='Tells the bot to join the voice channel')
+    @commands.slash_command(name='join', description='Tells the bot to join the voice channel')
     async def join(self, ctx):
         if not ctx.author.voice:
             await ctx.respond("{} is not connected to a voice channel".format(ctx.author.name))
@@ -42,7 +64,7 @@ class MusicCog(commands.Cog):
             else:
                 await ctx.respond("Already in a channel m8")
 
-    @discord.slash_command(name='leave', description='Tells the bot to leave the voice channel')
+    @commands.slash_command(name='leave', description='Tells the bot to leave the voice channel')
     async def leave(self, com):
         if com.voice_client is None:
             await com.respond("I ain't in a channel ya piker!")
@@ -50,9 +72,28 @@ class MusicCog(commands.Cog):
             if com.voice_client.channel is not com.author.voice.channel:
                 await com.respond("You ain't in my channel ya no-hoper!")
             else:
-                await com.voice_client.disconnect(force=True)
+                if com.voice_client.is_playing():
+                    com.voice_client.stop()
                 await com.respond("done")
+                await com.voice_client.disconnect(force=True)
 
+    @commands.slash_command(name='playlocal', description='Plays from local computer (functionality on systems without ffmpeg is uncertain [likely not])')
+    async def playlocal(self, ctx, *, query: str):
+        if ctx.voice_client is not None:
+            source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
+            ctx.voice_client.play(source, after=lambda e: print(f"Player error: {e}") if e else None)
+            await ctx.respond(f'Now playing {query}')
+        else:
+            await ctx.respond("aven't even joined ya booksmarts!")
+
+    @commands.slash_command(name='play', description='')
+    async def play(self, ctx: commands.Context, *, url: str):
+        async with ctx.typing():
+            player = await YTDLSource.from_url(url, loop=self.bot.loop)
+            ctx.voice_client.play(
+                player, after=lambda e: print(f"Player error: {e}") if e else None
+            )
+            await ctx.message.respond(f'Now playing: {player.title}')
 
 def setup(bot):
     bot.add_cog(MusicCog(bot))
