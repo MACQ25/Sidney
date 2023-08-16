569e3fc5a4477f64d58373c6ca39ee54bc37e00b
M. Canul <canulmauricio03@gmail.com>
2023-08-16 18:40:21 -0400

    Got it to run through multiple songs downloaded using yt-dl

--- cogs/MusicCog.py
+++ cogs/MusicCog.py
@@ -1,7 +1,10 @@
 import asyncio
 import discord
 import yt_dlp as youtube_dl
+from discord import VoiceProtocol
 from discord.ext import commands
+
+from main import ClientsData
 
 youtube_dl.utils.bug_reports_message = lambda: ""
 
@@ -34,46 +37,48 @@ class YTDLSource(discord.PCMVolumeTransformer):
         self.url = data.get("url")
 
     @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
+    async def from_url(cls, cId, url, *, loop=None, stream=False):
+        pl = []
         loop = loop or asyncio.get_event_loop()
         data = await loop.run_in_executor(
             None, lambda: ytdl.extract_info(url, download=not stream)
         )
-        if "entries" in data:
-            data = data["entries"][0]
 
-        filename = data["url"] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+        # Found it, if it is a playlist "entries" will be detected :D
+        if "entries" in data:
+
+            for entry in data["entries"]:
+                pl.append(
+                    {"title": entry.get("title"), "url": entry.get("url"), "filename": ytdl.prepare_filename(entry)})
+        return pl
+
+    @classmethod
+    def play_from_queue(cls, source: dict):
+        return cls(discord.FFmpegPCMAudio(source["filename"], **ffmpeg_options), data=source)
 
 
 class MusicCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.slash_command(name='join', description='Tells the bot to join the voice channel')
-    async def join(self, ctx):
-        if not ctx.author.voice:
-            await ctx.respond("{} is not connected to a voice channel".format(ctx.author.name))
+    def playListPlayer(self, ctx, gId):
+        if ClientsData.retrieve(gId, "Qn") < (len(ClientsData.retrieve(gId, "playlist")) - 1):
+            ClientsData.set(gId, ["Qn", ClientsData.retrieve(gId, "Qn") + 1])
+            entry = ClientsData.retrieve(gId, "playlist")[ClientsData.retrieve(gId, "Qn")]
+            player = YTDLSource.play_from_queue(entry)
+            ctx.voice_client.play(
+                player, after=lambda e: print(f"Player error: {e}") if e else self.playListPlayer(ctx, gId))
         else:
-            if ctx.voice_client is None:
-                await ctx.respond("cheers m8")
-                channel = ctx.author.voice.channel
-                await channel.connect()
-            else:
-                await ctx.respond("Already in a channel m8")
+            ClientsData.set(gId, ['CurrentlyPlaying', None], ["playlist", None], ["Qn", 0])
 
-    @commands.slash_command(name='leave', description='Tells the bot to leave the voice channel')
-    async def leave(self, com):
-        if com.voice_client is None:
-            await com.respond("I ain't in a channel ya piker!")
+    @commands.slash_command(name='join', description='Tells the bot to join the voice channel')
+    async def join(self, ctx, channel: discord.VoiceChannel):
+        if ctx.voice_client is not None:
+            ctx.respond("on the move")
+            await ctx.voice_client.move_to(channel)
         else:
-            if com.voice_client.channel is not com.author.voice.channel:
-                await com.respond("You ain't in my channel ya no-hoper!")
-            else:
-                if com.voice_client.is_playing():
-                    com.voice_client.stop()
-                await com.respond("done")
-                await com.voice_client.disconnect(force=True)
+            await ctx.respond("goin'")
+            await channel.connect()
 
     @commands.slash_command(name='playlocal',
                             description='Plays from local computer (functionality on systems without ffmpeg is uncertain [likely not])')
@@ -87,12 +92,71 @@ class MusicCog(commands.Cog):
 
     @commands.slash_command(name='play', description='')
     async def play(self, ctx, *, url: str):
-        async with ctx.typing():
-            player = await YTDLSource.from_url(url, loop=self.bot.loop)
-            await ctx.respond(f'Now playing: {player.title}')
-            ctx.voice_client.play(
-                player, after=lambda e: print(f"Player error: {e}") if e else None
-            )
+        gId = ctx.guild.id
+        if ctx.voice_client is not None and ctx.voice_client.is_connected():
+            async with ctx.typing():
+                await ctx.respond('On it...')
+                playlist = await YTDLSource.from_url(gId, url, loop=self.bot.loop)
+                ClientsData.set(gId, ['playlist', playlist])
+                player = YTDLSource.play_from_queue(playlist[ClientsData.retrieve(gId, "Qn")])
+                ctx.voice_client.play(
+                    player, after=lambda e: print(f"Player error: {e}") if e else self.playListPlayer(ctx, gId))
+                await ctx.send(f'Now playing: {player.title}')
+                ClientsData.set(ctx.guild.id, ["CurrentlyPlaying", player.title])
+        else:
+            await ctx.respond('Sorry mate, unable to do so')
+
+    @commands.slash_command(name='pause', description='Pauses the audio file currently being played by the bot')
+    async def pause(self, ctx):
+        if ctx.voice_client.is_playing():
+            ctx.voice_client.pause()
+            await ctx.respond(f'Paused it for ya!')
+        else:
+            await ctx.respond('Sorry mate, unable to do so')
+
+    @commands.slash_command(name='resume', description='Allows the current audio file in the queue to continue')
+    async def resume(self, ctx):
+        if ctx.voice_client.is_paused():
+            ctx.voice_client.resume()
+            await ctx.respond('here ya go')
+        else:
+            await ctx.respond('Sorry mate, unable to do so')
+
+    @commands.slash_command(name='skip', description='Removes the current track from the queue')
+    async def skip(self, ctx):
+        if ctx.voice_client.is_playing():
+            ctx.voice_client.stop()
+            await ctx.respond('onto the next one')
+            ClientsData.set(ctx.guild.id, ['CurrentlyPlaying', None])
+        else:
+            await ctx.respond('Sorry mate, unable to do so')
+
+    @commands.slash_command(name='empty', description='Empties the Queue in its entirety')
+    async def empty(self, ctx):
+        if ctx.voice_client.is_playing():
+            await ctx.respond('it is gone pal')
+            ClientsData.set(ctx.guild.id, ['CurrentlyPlaying', None], ['playlist', None], ['Qn', 0])
+            ctx.voice_client.stop()
+        else:
+            await ctx.respond('Sorry mate, unable to do so')
+
+    @commands.slash_command(name='leave', description='Tells the bot to leave the voice channel')
+    async def leave(self, ctx):
+        if ctx.voice_client is None:
+            await ctx.respond("I ain't in a channel ya piker!")
+        else:
+            if ctx.voice_client.channel is not ctx.author.voice.channel:
+                await ctx.respond("You ain't in my channel ya no-hoper!")
+            else:
+                if ctx.voice_client.is_playing():
+                    ctx.voice_client.stop()
+                    await discord.VoiceProtocol.disconnect(force=True)
+                await ctx.respond("done")
+                await ctx.voice_client.disconnect(force=True)
+
+    @commands.slash_command(name='sauce', description='gives sauce of song')
+    async def sauce(self, ctx):
+        await ctx.respond(f'it is {ClientsData.retrieve(ctx.guild.id, "CurrentlyPlaying")}')
 
 
 def setup(bot):
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import discord
-from discord import voice_client
+from discord import voice_client, Client
 from discord.ext import commands
 from discord import Guild
 import os
@@ -22,13 +22,56 @@ sidneyBot = commands.Bot(
 )
 
 
+def struct(guild):
+    newEnt = {
+        guild.id: {
+            "CurrentlyPlaying": None,
+            "playlist": None,
+            "Qn": 0,
+        }
+    }
+    return newEnt
+
+
+class ClientsData:
+    serverStatus = {}
+
+    @classmethod
+    async def dataBuilder(cls):
+        async for guild in sidneyBot.fetch_guilds():
+            cls.serverStatus.update(struct(guild))
+
+    @classmethod
+    def newEntry(cls, guild):
+        cls.serverStatus.update(struct(guild))
+
+    @classmethod
+    def retrieve(cls, gId, key):
+        if gId in cls.serverStatus and key in cls.serverStatus.get(gId):
+            return cls.serverStatus.get(gId).get(key)
+
+    @classmethod
+    def set(cls, gId, *keys: []):
+        if gId in cls.serverStatus:
+            for pair in keys:
+                if pair[0] in cls.serverStatus.get(gId):
+                    cls.serverStatus.get(gId).update({pair[0]: pair[1]})
+                else:
+                    print("not here")
+        else:
+            print("not dict")
+
+
 @sidneyBot.event
 async def on_ready():
+    await ClientsData.dataBuilder()
+    # print(ClientsData.serverStatus) debugger to check number of servers this is in
     print(f'{sidneyBot.user} says: God save the queen!')
 
 
 @sidneyBot.event
 async def on_guild_join(guild: discord.Guild):
+    ClientsData.newEntry(guild)
     for channel in guild.text_channels:
         if channel.permissions_for(guild.me).send_messages:
             if demoji.replace(channel.name.lower().strip(), "").find("general") != -1:
@@ -41,27 +84,22 @@ async def on_guild_join(guild: discord.Guild):
             return
 
 
-@sidneyBot.slash_command(description="Say Hi to another user on the server")
-async def salute(ctx, msg):
-    await ctx.respond(f'cheers {msg}')
-
-
-@sidneyBot.slash_command()
-async def add(ctx, left: int, right: int):
-    await ctx.respond(str(left + right))
+@sidneyBot.event
+async def on_guild_remove(guild: discord.Guild):
+    ClientsData.serverStatus.pop(guild.id)
 
 
 for res in os.listdir("cogs"):
     if res.endswith(".py"):
         sidneyBot.load_extension(f'cogs.{res.replace(".py", "")}')
 
-sidneyBot.run('MTEyNDEyMDgyNDI0NTM5MTQzNA.GMOCsF.M2PNcbAYuJVgCynPOszXLuBFQ6dNIBqLf8sg8s')
+sidneyBot.run('')
 
 # Sources, some good, some unused:
 
 # sauce for async kerfuffle solu: https://stackoverflow.com/questions/71504627/runtimewarning-coroutine-botbase-load-extension-was-never-awaited-after-upd
 
-# Need to add the command cog to the CLIENT as said: https://stackoverflow.com/questions/71960548/bot-launches-fine-but-the-commands-do-not-work?rq=3
+# Need to add the command cog to the CLIENT as said: https://stackoverflow.com/questions/71960548/bot-launches-fine-but-the-commands-do-not-w/ork?rq=3
 
 # Kind gentleman that helped explain why "load_extension()" and "setup()" were not working
 # changed sidney = discord.Bot() to sidney = commands.Bot() as commands and support cogs and discord doesnt
